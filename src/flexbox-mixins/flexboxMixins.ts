35725aad7f5829bd8cd5d32b5942cac6c3797be4
Dani Rocha Azevedo <azevedorocha.dani@gmail.com>
2023-11-21 13:31:07 +0100

    remove bad gap mixin

--- src/flexbox-mixins/flexboxMixins.ts
+++ src/flexbox-mixins/flexboxMixins.ts
@@ -47,15 +47,6 @@ export const flex = (value: number) => css`
 	flex: ${value};
 `
 
-export const gap = (value: string) => css`
-	> * {
-		margin-right: ${value};
-		&:last-child {
-			margin-right: 0;
-		}
-	}
-`
-
 export const flexGrow = (value: number) => css`
 	-webkit-box-flex: ${value};
 	-webkit-flex-grow: ${value};
